3987e2f637e081cd2b614c18532819f5c67c81d6
Zydsonek <98161953+Zydson@users.noreply.github.com>
2025-12-10 20:38:54 +0100

    -

--- main.py
+++ main.py
@@ -6,20 +6,11 @@ from modules.notepad import notepad_bp
 from modules.admin import admin_bp
 from modules.errors import errors_bp
 from modules.files import files_bp
-
-#Building
-#for x in os.listdir("html"):
-#    for y in os.listdir(f"html/{x}"):
-#        if ".js" in y:
-#            open(f"build/{y}","w+").write(jsmin(open(f"html/{x}/{y}","r",errors="ignore").read(),keep_bang_comments=False))
-#        elif ".css" in y:
-#            process_single_css_file(f"html/{x}/{y}",overwrite=False,output_path=f"build/{y}")
-#        elif ".html" in y:
-#            process_single_html_file(f"html/{x}/{y}",overwrite=False,output_path=f"build/{y}")
+from globals import MAX_CONTENT_LENGTH
 
 app = Flask("rb",static_folder=None)
 app.template_folder = "html"
-app.config['MAX_CONTENT_LENGTH'] = 5000 * 1024 * 1024
+app.config['MAX_CONTENT_LENGTH'] = MAX_CONTENT_LENGTH
 app.config['UPLOAD_FOLDER'] = os.path.join(os.getcwd(), 'accounts')
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 
