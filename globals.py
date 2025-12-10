8ab4ba99788804c56ba3665a7b2fc2b7aa91478c
Zydsonek <98161953+Zydson@users.noreply.github.com>
2025-12-10 20:38:20 +0100

    -

--- globals.py
+++ globals.py
@@ -1,12 +1,14 @@
-import json, bcrypt, random, string, time, os, datetime, threading, subprocess, psutil, requests, shutil, hashlib, zipfile, rarfile, tarfile
+import json, bcrypt, random, string, time, os, datetime, threading, subprocess, psutil, requests, shutil, hashlib, zipfile, rarfile, tarfile, uuid
 from flask import Flask, render_template, send_file, request, send_from_directory, redirect, Blueprint, current_app, jsonify, make_response
 
 from werkzeug.exceptions import RequestEntityTooLarge
-from werkzeug.datastructures import ImmutableMultiDict # Cookies handle
+from werkzeug.datastructures import ImmutableMultiDict
 from werkzeug.utils import secure_filename
 
 accJson = open('accounts/accounts.json','r+')
 Accounts = json.load(accJson)
+    
+MAX_CONTENT_LENGTH = 1024 * 1024 * 100 #MB
 
 def allowed(req):
     try:
@@ -22,5 +24,8 @@ def updateJson():
     tempAccounts = Accounts.copy()
     json.dump(tempAccounts, open('accounts/accounts.json','w+'), indent = 4)
 
+if "salt" not in Accounts:
+    Accounts["salt"] = bcrypt.gensalt().decode()
+    updateJson()
 
-__all__ = ['json', 'bcrypt', 'random', 'string', 'time', 'os', 'datetime', 'threading', 'subprocess', 'hashlib', 'psutil', 'shutil', 'updateJson', 'Accounts', 'requests', 'RequestEntityTooLarge', 'ImmutableMultiDict', 'secure_filename', 'Flask', 'render_template', 'send_file', 'request', 'send_from_directory', 'redirect', 'Blueprint', 'current_app', 'allowed', 'jsonify', 'make_response', 'zipfile', 'rarfile', 'tarfile']
+__all__ = ['json', 'bcrypt', 'random', 'string', 'time', 'os', 'datetime', 'threading', 'subprocess', 'hashlib', 'psutil', 'shutil', 'updateJson', 'Accounts', 'requests', 'RequestEntityTooLarge', 'ImmutableMultiDict', 'secure_filename', 'Flask', 'render_template', 'send_file', 'request', 'send_from_directory', 'redirect', 'Blueprint', 'current_app', 'allowed', 'jsonify', 'make_response', 'zipfile', 'rarfile', 'tarfile', 'uuid']
